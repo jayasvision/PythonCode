2367a09b19da87bdcfb6270fe088072dc0d34ff9
Abhimanyu <abhirathore2006@gmail.com>
2017-11-19 23:33:22 +0530

    added redux form and bootstrap

--- web/src/client/client.jsx
+++ web/src/client/client.jsx
@@ -1,22 +1,27 @@
-import * as React from 'react'
-import {render}  from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router'
-import { Provider } from 'react-redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import configureStore from '../store/configureStore';
+import * as React from "react";
+import { render } from "react-dom";
+import { Router, Route, browserHistory } from "react-router";
+import { Provider } from "react-redux";
+import { syncHistoryWithStore } from "react-router-redux";
+import configureStore from "../store/configureStore";
 
-import App from '../components/app/App'
-import Home from '../components/home/Home'
-import Login from '../components/login/Login'
-import { requireAuth } from '../utils/AuthWrapper';
+import App from "../components/app/App";
+import Home from "../components/home/Home";
+import Login from "../components/login/Login";
+import { requireAuth } from "../utils/AuthWrapper";
+require("bootstrap/dist/css/bootstrap.css");
+
 const store = configureStore(browserHistory, {});
 const enhancedHistory = syncHistoryWithStore(browserHistory, store);
-let routes =(
-    <Route path="/" component={App}>
-        <Route path="/home" component={requireAuth(Home,"/login")} />
-        <Route path="/login" component={Login} />
-    </Route>
-)
-render( (<Provider store={store}>
-          <Router history={enhancedHistory} routes={routes} />
-      </Provider>) ,document.getElementById("react-root"))
+let routes = (
+	<Route path="/" component={App}>
+		<Route path="/home" component={requireAuth(Home, "/login")} />
+		<Route path="/login" component={Login} />
+	</Route>
+);
+render(
+	<Provider store={store}>
+		<Router history={enhancedHistory} routes={routes} />
+	</Provider>,
+	document.getElementById("react-root")
+);
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,32 +1,31 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
 	debug: true,
-	devtool: 'inline-source-map',
-	entry: [
-		'webpack-hot-middleware/client?reload=true',
-		'./src/client/client.jsx'
-	],
+	devtool: "inline-source-map",
+	entry: ["webpack-hot-middleware/client?reload=true", "./src/client/client.jsx"],
 	output: {
-		path: path.join(__dirname, './src/client/assets'),
-		filename: 'bundle.js',
+		path: path.join(__dirname, "./src/client/assets"),
+		filename: "bundle.js",
 		publicPath: "/"
 	},
-	plugins:[
-		new webpack.HotModuleReplacementPlugin()
-	],
+	plugins: [new webpack.HotModuleReplacementPlugin()],
 	module: {
 		loaders: [
 			{
 				test: /\.js(x?)$/,
-				loaders: ['babel-loader?cacheDirectory,presets[]=react,presets[]=es2015,presets[]=stage-0'],
-				include: path.join(__dirname, 'src')
+				loaders: ["babel-loader?cacheDirectory,presets[]=react,presets[]=es2015,presets[]=stage-0"],
+				include: path.join(__dirname, "src")
+			},
+			{
+				test: /\.css$/,
+				loader: "style-loader!css-loader"
 			}
-    ]
+		]
 	},
 	resolve: {
-		root: [path.resolve('./src')],
-		extensions: ['', '.jsx', '.js']
+		root: [path.resolve("./src"), path.resolve("./node_modules")],
+		extensions: ["", ".jsx", ".js"]
 	}
 };
